2f1200f7497643231d5e37ca419f6eb5e2156f84
fake-stmz <artur.kravchenko.2006@gmail.com>
2025-05-28 19:07:32 +0300

    catalogue done -> COMPLETE

--- webfigurines/figurinehub/views.py
+++ webfigurines/figurinehub/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from .models import Figurine
 
 def index(request):
     return render(request, 'index.html')
 
 def catalogue(request):
-    return render(request, 'catalogue.html')
+    figurines = Figurine.objects.all()
+    
+    # Поиск
+    search = request.GET.get('search')
+    if search:
+        figurines = figurines.filter(name__icontains=search)
+    
+    # Сортировка
+    sort = request.GET.get('sort')
+    if sort == 'price_asc':
+        figurines = figurines.order_by('price')
+    elif sort == 'price_desc':
+        figurines = figurines.order_by('-price')
+    elif sort == 'name_asc':
+        figurines = figurines.order_by('name')
+    elif sort == 'name_desc':
+        figurines = figurines.order_by('-name')
+    
+    context = {
+        'figurines': figurines,
+        'search': search,
+        'sort': sort,
+    }
+        
+    return render(request, 'catalogue.html', context)
